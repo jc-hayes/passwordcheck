b5ebaae8103abe123cd3a40cbfeae7bdeeb7410f
James H <jchayes86@gmail.com>
2019-03-07 12:42:42 -0800

    two-way-binding TODO's

--- app/src/main/java/edu/cascadia/mobile/apps/passwordcheck/MainActivity.java
+++ app/src/main/java/edu/cascadia/mobile/apps/passwordcheck/MainActivity.java
@@ -2,6 +2,7 @@ package edu.cascadia.mobile.apps.passwordcheck;
 
 import android.arch.lifecycle.ViewModel;
 import android.arch.lifecycle.ViewModelProvider;
+import android.database.DatabaseUtils;
 import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -10,20 +11,23 @@ import edu.cascadia.mobile.apps.passwordcheck.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
-    //TODO: create a private ViewModel instance variable
+    //create a private ViewModel instance variable
+    private PasswordViewModel passwordViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //Comment out the setContentView - we'll use a binding instead
-        setContentView(R.layout.activity_main);
+        //setContentView(R.layout.activity_main);
 
-        //TODO: Create a new binding using DataBindingUtil
+        //Create a new binding using DataBindingUtil
+        ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
 
-        //TODO: Create a new PasswordViewModel object
+        //Create a new PasswordViewModel object
+        passwordViewModel = new PasswordViewModel();
 
-        //TODO: Set the binding's ViewModel to your new PasswordViewModel
-
+        //Set the binding's ViewModel to your new PasswordViewModel
+        binding.setViewModel(passwordViewModel);
 
     }
 }
